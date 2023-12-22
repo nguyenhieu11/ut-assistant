ee300f93e0332fa960e918a71a0ddc0ea2e7d7d0
nguyenhieu11 <nt.hieu.1911@gmail.com>
2023-12-22 14:00:36 +0700

    new template of test script

--- source-structure/E2E_P05/test_E2E_P05/test_E2E_P05.cpp
+++ source-structure/E2E_P05/test_E2E_P05/test_E2E_P05.cpp
@@ -19,6 +19,10 @@ using ::testing::ReturnRef;
 
 //==================END_AUTO_EXTERN_FUNCTION==================//
 
+//==================BEGIN_AUTO_EXTERN_STUB_FUNCTION==================//
+
+//==================END_AUTO_EXTERN_STUB_FUNCTION==================//
+
 /* Extern variable */
 //==================BEGIN_AUTO_EXTERN_VARIABLE==================//
 
@@ -28,6 +32,9 @@ using ::testing::ReturnRef;
 class Stub
 {
 public:
+    //==================BEGIN_AUTO_MOCK_STUB_FUNCTION==================//
+
+    //==================END_AUTO_MOCK_STUB_FUNCTION==================//
     Stub()
     {
         s_instance = this;
@@ -36,6 +43,9 @@ public:
 };
 Stub *Stub::s_instance;
 
+//==================BEGIN_AUTO_DEFINE_STUB_FUNCTION==================//
+
+//==================END_AUTO_DEFINE_STUB_FUNCTION==================//
 namespace Test
 {
     class ClassUnitTest : public ::testing::Test
