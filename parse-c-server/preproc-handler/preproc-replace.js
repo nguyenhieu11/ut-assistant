baa7c7db0f3a5faf255ecedec6d8ac600229bd82
nguyenhieu11 <nt.hieu.1911@gmail.com>
2024-03-03 16:27:21 +0700

    add replace preproc with value method

--- parse-c-server/preproc-handler/preproc-replace.js
+++ parse-c-server/preproc-handler/preproc-replace.js
@@ -245,3 +245,14 @@ export async function test_function() {
 }
 
 
+const replacePreproc = async (str, replace_with) => {
+    const spaceSurrounded = /\s+xxx\s+/g;
+    const parenSurrounded = /\(\s*xxx\s*\)/g;
+    const specialCharSurrounded = /(?<=\s|\(|\&|\||\!|=)xxx(?=\s|\)|\&|\||\!|=)/g;
+
+    // let str = ' xxx , (xxx), &xxx| !xxx =xxx xxx= ';
+    str = str.replace(spaceSurrounded, replace_with)
+        .replace(parenSurrounded, replace_with)
+        .replace(specialCharSurrounded, replace_with);
+    console.log(str); // Outputs: ' yyy , (yyy), &yyy| !yyy =yyy yyy= '
+};
